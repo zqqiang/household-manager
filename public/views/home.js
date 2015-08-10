b19127fca618fe3c9d863debd50dece4aab0b91d
qiangzhaoqing <qiangzhaoqing@gmail.com>
2015-08-10 09:21:31 -0700

    add move cube demo

--- public/views/home.js
+++ public/views/home.js
@@ -3,7 +3,8 @@ define(['marionette', 'templates/compiled'], function(Marionette, JST) {
 		template: JST.HomeTemplate,
 		className: 'home-page',
 		onShow: function() {
-			this.demoCube();
+			// this.demoCube();
+			this.movingCube();
 		},
 		demoCube: function() {
 			var renderer = new THREE.WebGLRenderer();
@@ -64,309 +65,309 @@ define(['marionette', 'templates/compiled'], function(Marionette, JST) {
 			gui.addColor(control, 'color');
 			return control;
 		},
-		// movingCube: function() {
+		movingCube: function() {
 
-		// 	/**
-		// 	 * @author alteredq / http://alteredqualia.com/
-		// 	 * @author mr.doob / http://mrdoob.com/
-		// 	 */
+			/**
+			 * @author alteredq / http://alteredqualia.com/
+			 * @author mr.doob / http://mrdoob.com/
+			 */
 
-		// 	var Detector = {
+			var Detector = {
 
-		// 		canvas: !!window.CanvasRenderingContext2D,
-		// 		webgl: (function() {
-		// 			try {
-		// 				var canvas = document.createElement('canvas');
-		// 				return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
-		// 			} catch (e) {
-		// 				return false;
-		// 			}
-		// 		})(),
-		// 		workers: !!window.Worker,
-		// 		fileapi: window.File && window.FileReader && window.FileList && window.Blob,
+				canvas: !!window.CanvasRenderingContext2D,
+				webgl: (function() {
+					try {
+						var canvas = document.createElement('canvas');
+						return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
+					} catch (e) {
+						return false;
+					}
+				})(),
+				workers: !!window.Worker,
+				fileapi: window.File && window.FileReader && window.FileList && window.Blob,
 
-		// 		getWebGLErrorMessage: function() {
+				getWebGLErrorMessage: function() {
 
-		// 			var element = document.createElement('div');
-		// 			element.id = 'webgl-error-message';
-		// 			element.style.fontFamily = 'monospace';
-		// 			element.style.fontSize = '13px';
-		// 			element.style.fontWeight = 'normal';
-		// 			element.style.textAlign = 'center';
-		// 			element.style.background = '#fff';
-		// 			element.style.color = '#000';
-		// 			element.style.padding = '1.5em';
-		// 			element.style.width = '400px';
-		// 			element.style.margin = '5em auto 0';
+					var element = document.createElement('div');
+					element.id = 'webgl-error-message';
+					element.style.fontFamily = 'monospace';
+					element.style.fontSize = '13px';
+					element.style.fontWeight = 'normal';
+					element.style.textAlign = 'center';
+					element.style.background = '#fff';
+					element.style.color = '#000';
+					element.style.padding = '1.5em';
+					element.style.width = '400px';
+					element.style.margin = '5em auto 0';
 
-		// 			if (!this.webgl) {
+					if (!this.webgl) {
 
-		// 				element.innerHTML = window.WebGLRenderingContext ? [
-		// 					'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
-		// 					'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
-		// 				].join('\n') : [
-		// 					'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
-		// 					'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
-		// 				].join('\n');
+						element.innerHTML = window.WebGLRenderingContext ? [
+							'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
+							'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+						].join('\n') : [
+							'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
+							'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+						].join('\n');
 
-		// 			}
+					}
 
-		// 			return element;
+					return element;
 
-		// 		},
+				},
 
-		// 		addGetWebGLMessage: function(parameters) {
+				addGetWebGLMessage: function(parameters) {
 
-		// 			var parent, id, element;
+					var parent, id, element;
 
-		// 			parameters = parameters || {};
+					parameters = parameters || {};
 
-		// 			parent = parameters.parent !== undefined ? parameters.parent : document.body;
-		// 			id = parameters.id !== undefined ? parameters.id : 'oldie';
+					parent = parameters.parent !== undefined ? parameters.parent : document.body;
+					id = parameters.id !== undefined ? parameters.id : 'oldie';
 
-		// 			element = Detector.getWebGLErrorMessage();
-		// 			element.id = id;
+					element = Detector.getWebGLErrorMessage();
+					element.id = id;
 
-		// 			parent.appendChild(element);
+					parent.appendChild(element);
 
-		// 		}
+				}
 
-		// 	};
+			};
 
-		// 	// browserify support
-		// 	if (typeof module === 'object') {
+			// browserify support
+			if (typeof module === 'object') {
 
-		// 		module.exports = Detector;
+				module.exports = Detector;
 
-		// 	}
+			}
 
-		// 	if (!Detector.webgl) Detector.addGetWebGLMessage();
+			if (!Detector.webgl) Detector.addGetWebGLMessage();
 
-		// 	var container;
-		// 	var camera, scene, renderer;
-		// 	var plane, cube;
-		// 	var mouse, raycaster, isShiftDown = false;
+			var container;
+			var camera, scene, renderer;
+			var plane, cube;
+			var mouse, raycaster, isShiftDown = false;
 
-		// 	var rollOverMesh, rollOverMaterial;
-		// 	var cubeGeo, cubeMaterial;
+			var rollOverMesh, rollOverMaterial;
+			var cubeGeo, cubeMaterial;
 
-		// 	var objects = [];
+			var objects = [];
 
-		// 	init();
-		// 	render();
+			init();
+			render();
 
-		// 	function init() {
+			function init() {
 
-		// 		// container = document.createElement('div');
-		// 		// document.body.appendChild(container);
+				// container = document.createElement('div');
+				// document.body.appendChild(container);
 
-		// 		// var info = document.createElement('div');
-		// 		// // info.style.position = 'absolute';
-		// 		// info.style.top = '10px';
-		// 		// info.style.width = '100%';
-		// 		// info.style.textAlign = 'center';
-		// 		// info.innerHTML = '<a href="http://threejs.org" target="_blank">three.js</a> - voxel painter - webgl<br><strong>click</strong>: add voxel, <strong>shift + click</strong>: remove voxel';
-		// 		// container.appendChild(info);
+				// var info = document.createElement('div');
+				// // info.style.position = 'absolute';
+				// info.style.top = '10px';
+				// info.style.width = '100%';
+				// info.style.textAlign = 'center';
+				// info.innerHTML = '<a href="http://threejs.org" target="_blank">three.js</a> - voxel painter - webgl<br><strong>click</strong>: add voxel, <strong>shift + click</strong>: remove voxel';
+				// container.appendChild(info);
 
-		// 		camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-		// 		camera.position.set(500, 800, 1300);
-		// 		camera.lookAt(new THREE.Vector3());
+				camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+				camera.position.set(500, 800, 1300);
+				camera.lookAt(new THREE.Vector3());
 
-		// 		scene = new THREE.Scene();
+				scene = new THREE.Scene();
 
-		// 		// roll-over helpers
+				// roll-over helpers
 
-		// 		rollOverGeo = new THREE.BoxGeometry(50, 50, 50);
-		// 		rollOverMaterial = new THREE.MeshBasicMaterial({
-		// 			color: 0xff0000,
-		// 			opacity: 0.5,
-		// 			transparent: true
-		// 		});
-		// 		rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial);
-		// 		scene.add(rollOverMesh);
+				rollOverGeo = new THREE.BoxGeometry(50, 50, 50);
+				rollOverMaterial = new THREE.MeshBasicMaterial({
+					color: 0xff0000,
+					opacity: 0.5,
+					transparent: true
+				});
+				rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial);
+				scene.add(rollOverMesh);
 
-		// 		// cubes
+				// cubes
 
-		// 		cubeGeo = new THREE.BoxGeometry(50, 50, 50);
-		// 		cubeMaterial = new THREE.MeshLambertMaterial({
-		// 			color: 0xfeb74c,
-		// 			shading: THREE.FlatShading,
-		// 			map: THREE.ImageUtils.loadTexture("theme/project/img/square-outline-textured.png")
-		// 		});
+				cubeGeo = new THREE.BoxGeometry(50, 50, 50);
+				cubeMaterial = new THREE.MeshLambertMaterial({
+					color: 0xfeb74c,
+					shading: THREE.FlatShading,
+					map: THREE.ImageUtils.loadTexture("theme/project/img/square-outline-textured.png")
+				});
 
-		// 		// grid
+				// grid
 
-		// 		var size = 500,
-		// 			step = 50;
+				var size = 500,
+					step = 50;
 
-		// 		var geometry = new THREE.Geometry();
+				var geometry = new THREE.Geometry();
 
-		// 		for (var i = -size; i <= size; i += step) {
+				for (var i = -size; i <= size; i += step) {
 
-		// 			geometry.vertices.push(new THREE.Vector3(-size, 0, i));
-		// 			geometry.vertices.push(new THREE.Vector3(size, 0, i));
+					geometry.vertices.push(new THREE.Vector3(-size, 0, i));
+					geometry.vertices.push(new THREE.Vector3(size, 0, i));
 
-		// 			geometry.vertices.push(new THREE.Vector3(i, 0, -size));
-		// 			geometry.vertices.push(new THREE.Vector3(i, 0, size));
+					geometry.vertices.push(new THREE.Vector3(i, 0, -size));
+					geometry.vertices.push(new THREE.Vector3(i, 0, size));
 
-		// 		}
+				}
 
-		// 		var material = new THREE.LineBasicMaterial({
-		// 			color: 0x000000,
-		// 			opacity: 0.2,
-		// 			transparent: true
-		// 		});
+				var material = new THREE.LineBasicMaterial({
+					color: 0x000000,
+					opacity: 0.2,
+					transparent: true
+				});
 
-		// 		var line = new THREE.Line(geometry, material, THREE.LinePieces);
-		// 		scene.add(line);
+				var line = new THREE.Line(geometry, material, THREE.LinePieces);
+				scene.add(line);
 
-		// 		raycaster = new THREE.Raycaster();
-		// 		mouse = new THREE.Vector2();
+				raycaster = new THREE.Raycaster();
+				mouse = new THREE.Vector2();
 
-		// 		var geometry = new THREE.PlaneBufferGeometry(1000, 1000);
-		// 		geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+				var geometry = new THREE.PlaneBufferGeometry(1000, 1000);
+				geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
 
-		// 		plane = new THREE.Mesh(geometry);
-		// 		plane.visible = false;
-		// 		scene.add(plane);
+				plane = new THREE.Mesh(geometry);
+				plane.visible = false;
+				scene.add(plane);
 
-		// 		objects.push(plane);
+				objects.push(plane);
 
-		// 		// Lights
+				// Lights
 
-		// 		var ambientLight = new THREE.AmbientLight(0x606060);
-		// 		scene.add(ambientLight);
+				var ambientLight = new THREE.AmbientLight(0x606060);
+				scene.add(ambientLight);
 
-		// 		var directionalLight = new THREE.DirectionalLight(0xffffff);
-		// 		directionalLight.position.set(1, 0.75, 0.5).normalize();
-		// 		scene.add(directionalLight);
+				var directionalLight = new THREE.DirectionalLight(0xffffff);
+				directionalLight.position.set(1, 0.75, 0.5).normalize();
+				scene.add(directionalLight);
 
-		// 		renderer = new THREE.WebGLRenderer({
-		// 			antialias: true
-		// 		});
-		// 		renderer.setClearColor(0xf0f0f0);
-		// 		renderer.setPixelRatio(window.devicePixelRatio);
-		// 		renderer.setSize( /*window.innerWidth*/ 800, /*window.innerHeight*/ 660);
+				renderer = new THREE.WebGLRenderer({
+					antialias: true
+				});
+				renderer.setClearColor(0xf0f0f0);
+				renderer.setPixelRatio(window.devicePixelRatio);
+				renderer.setSize( /*window.innerWidth*/ 800, /*window.innerHeight*/ 660);
 
-		// 		// container.appendChild(renderer.domElement);
-		// 		$('div.3d').html(renderer.domElement);
+				// container.appendChild(renderer.domElement);
+				$('div.3d').html(renderer.domElement);
 
-		// 		document.addEventListener('mousemove', onDocumentMouseMove, false);
-		// 		document.addEventListener('mousedown', onDocumentMouseDown, false);
-		// 		document.addEventListener('keydown', onDocumentKeyDown, false);
-		// 		document.addEventListener('keyup', onDocumentKeyUp, false);
+				document.addEventListener('mousemove', onDocumentMouseMove, false);
+				document.addEventListener('mousedown', onDocumentMouseDown, false);
+				document.addEventListener('keydown', onDocumentKeyDown, false);
+				document.addEventListener('keyup', onDocumentKeyUp, false);
 
-		// 		window.addEventListener('resize', onWindowResize, false);
+				window.addEventListener('resize', onWindowResize, false);
 
-		// 	}
+			}
 
-		// 	function onWindowResize() {
+			function onWindowResize() {
 
-		// 		camera.aspect = window.innerWidth / window.innerHeight;
-		// 		camera.updateProjectionMatrix();
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
 
-		// 		renderer.setSize(window.innerWidth, window.innerHeight);
+				renderer.setSize(window.innerWidth, window.innerHeight);
 
-		// 	}
+			}
 
-		// 	function onDocumentMouseMove(event) {
+			function onDocumentMouseMove(event) {
 
-		// 		event.preventDefault();
+				event.preventDefault();
 
-		// 		mouse.set((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1);
+				mouse.set((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1);
 
-		// 		raycaster.setFromCamera(mouse, camera);
+				raycaster.setFromCamera(mouse, camera);
 
-		// 		var intersects = raycaster.intersectObjects(objects);
+				var intersects = raycaster.intersectObjects(objects);
 
-		// 		if (intersects.length > 0) {
+				if (intersects.length > 0) {
 
-		// 			var intersect = intersects[0];
+					var intersect = intersects[0];
 
-		// 			rollOverMesh.position.copy(intersect.point).add(intersect.face.normal);
-		// 			rollOverMesh.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
+					rollOverMesh.position.copy(intersect.point).add(intersect.face.normal);
+					rollOverMesh.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
 
-		// 		}
+				}
 
-		// 		render();
+				render();
 
-		// 	}
+			}
 
-		// 	function onDocumentMouseDown(event) {
+			function onDocumentMouseDown(event) {
 
-		// 		event.preventDefault();
+				event.preventDefault();
 
-		// 		mouse.set((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1);
+				mouse.set((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1);
 
-		// 		raycaster.setFromCamera(mouse, camera);
+				raycaster.setFromCamera(mouse, camera);
 
-		// 		var intersects = raycaster.intersectObjects(objects);
+				var intersects = raycaster.intersectObjects(objects);
 
-		// 		if (intersects.length > 0) {
+				if (intersects.length > 0) {
 
-		// 			var intersect = intersects[0];
+					var intersect = intersects[0];
 
-		// 			// delete cube
+					// delete cube
 
-		// 			if (isShiftDown) {
+					if (isShiftDown) {
 
-		// 				if (intersect.object != plane) {
+						if (intersect.object != plane) {
 
-		// 					scene.remove(intersect.object);
+							scene.remove(intersect.object);
 
-		// 					objects.splice(objects.indexOf(intersect.object), 1);
+							objects.splice(objects.indexOf(intersect.object), 1);
 
-		// 				}
+						}
 
-		// 				// create cube
+						// create cube
 
-		// 			} else {
+					} else {
 
-		// 				var voxel = new THREE.Mesh(cubeGeo, cubeMaterial);
-		// 				voxel.position.copy(intersect.point).add(intersect.face.normal);
-		// 				voxel.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
-		// 				scene.add(voxel);
+						var voxel = new THREE.Mesh(cubeGeo, cubeMaterial);
+						voxel.position.copy(intersect.point).add(intersect.face.normal);
+						voxel.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
+						scene.add(voxel);
 
-		// 				objects.push(voxel);
+						objects.push(voxel);
 
-		// 			}
+					}
 
-		// 			render();
+					render();
 
-		// 		}
+				}
 
-		// 	}
+			}
 
-		// 	function onDocumentKeyDown(event) {
+			function onDocumentKeyDown(event) {
 
-		// 		switch (event.keyCode) {
+				switch (event.keyCode) {
 
-		// 			case 16:
-		// 				isShiftDown = true;
-		// 				break;
+					case 16:
+						isShiftDown = true;
+						break;
 
-		// 		}
+				}
 
-		// 	}
+			}
 
-		// 	function onDocumentKeyUp(event) {
+			function onDocumentKeyUp(event) {
 
-		// 		switch (event.keyCode) {
+				switch (event.keyCode) {
 
-		// 			case 16:
-		// 				isShiftDown = false;
-		// 				break;
+					case 16:
+						isShiftDown = false;
+						break;
 
-		// 		}
+				}
 
-		// 	}
+			}
 
-		// 	function render() {
+			function render() {
 
-		// 		renderer.render(scene, camera);
+				renderer.render(scene, camera);
 
-		// 	}
-		// }
+			}
+		}
 	});
 	return Home;
 });

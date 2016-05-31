d0fb008a1120ad4f71369d41fffee657a2c0df3c
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-05-30 17:36:17 -0700

    add toolbar android

--- app/index.android.js
+++ app/index.android.js
@@ -10,105 +10,33 @@ import {
     ListView,
     StyleSheet,
     Text,
-    View
+    View,
+    ToolbarAndroid
 } from 'react-native';
 
-var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
-
 class app extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dataSource: new ListView.DataSource({
-                rowHasChanged: (row1, row2) => row1 !== row2,
-            }),
-            loaded: false,
-        };
-    }
-
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    fetchData() {
-        fetch(REQUEST_URL)
-            .then((response) => response.json())
-            .then((responseData) => {
-                this.setState({
-                    dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-                    loaded: true,
-                });
-            })
-            .done();
-    }
-
     render() {
-        if (!this.state.loaded) {
-            return this.renderLoadingView();
-        }
-
+        var navigationView = (
+            <View style={{flex: 1, backgroundColor: '#fff'}}>
+                <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
+            </View>
+        );
         return (
-            <ListView
-                dataSource={this.state.dataSource}
-                renderRow={this.renderMovie}
-                style={styles.listView}
+            <ToolbarAndroid 
+                navIcon={require('./flux.png')} 
+                style={styles.toolbar} 
+                subtitle={'Example app with toolbar component - subtitle'}
+                title="Toolbar"
             />
-        );
-    }
-
-    renderLoadingView() {
-        return (
-            <View style={styles.container}>
-                <Text>
-                    Loading movies...
-                </Text>
-            </View>
-        );
-    }
-
-    renderMovie(movie) {
-        return (
-            <View style={styles.container}>
-                <Image
-                    source={{uri: movie.posters.thumbnail}}
-                    style={styles.thumbnail}
-                />
-                <View style={styles.rightContainer}>
-                    <Text style={styles.title}>{movie.title}</Text>
-                    <Text style={styles.year}>{movie.year}</Text>
-                </View>
-            </View>
         );
     }
 }
 
 var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    rightContainer: {
-        flex: 1,
-    },
-    title: {
-        fontSize: 20,
-        marginBottom: 8,
-        textAlign: 'center',
-    },
-    year: {
-        textAlign: 'center',
-    },
-    thumbnail: {
-        width: 53,
-        height: 81,
-    },
-    listView: {
-        paddingTop: 20,
-        backgroundColor: '#F5FCFF',
-    },
+  toolbar: {
+    backgroundColor: '#e9eaed',
+    height: 56,
+  },
 });
 
 AppRegistry.registerComponent('app', () => app);

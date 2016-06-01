8954d0abf99f7fc3e18480569f3441f2ad49e569
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-06-01 11:05:16 -0700

    add navigator

--- app/index.android.js
+++ app/index.android.js
@@ -11,32 +11,44 @@ import {
     StyleSheet,
     Text,
     View,
-    ToolbarAndroid
+    Navigator
 } from 'react-native';
+
+var RouteMapper = function(route, navigator) {
+    if (route.name === 'search') {
+        return (
+            <View style={{flex: 1}}>
+                <Text onPress={() => navigator.push({name:'other'})} >Search</Text>
+            </View>
+        );
+    } else if (route.name === 'other') {
+        return (
+            <View style={{flex: 1}}>
+                <Text onPress={() => {navigator.pop()}}>Other</Text>
+            </View>
+        );
+    }
+};
 
 class app extends Component {
     render() {
-        var navigationView = (
-            <View style={{flex: 1, backgroundColor: '#fff'}}>
-                <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
-            </View>
-        );
+        var initialRoute = { name: 'search' };
         return (
-            <ToolbarAndroid 
-                navIcon={require('./flux.png')} 
-                style={styles.toolbar} 
-                subtitle={'Example app with toolbar component - subtitle'}
-                title="Toolbar"
+            <Navigator
+                style={styles.container}
+                initialRoute={initialRoute}
+                configureScene={() => Navigator.SceneConfigs.FadeAndroid}
+                renderScene={RouteMapper}
             />
         );
     }
-}
+};
 
 var styles = StyleSheet.create({
-  toolbar: {
-    backgroundColor: '#e9eaed',
-    height: 56,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
 });
 
 AppRegistry.registerComponent('app', () => app);

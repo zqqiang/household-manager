7b88ff0e4516e066a0894b5fc41609cbcd92e04b
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-07-07 09:11:28 -0700

    add search icon

--- app/SearchBar.js
+++ app/SearchBar.js
@@ -5,13 +5,31 @@ import {
     Image,
     StyleSheet,
     TextInput,
+    Platform,
+    TouchableNativeFeedback
 } from 'react-native';
+
+var IS_RIPPLE_EFFECT_SUPPORTED = Platform.Version >= 21;
 
 class SearchBar extends React.Component {
     render() {
+        var background = IS_RIPPLE_EFFECT_SUPPORTED ?
+            TouchableNativeFeedback.SelectableBackgroundBorderless() :
+            TouchableNativeFeedback.SelectableBackground();
         return (
             <View style={styles.searchBar}>
+                <TouchableNativeFeedback
+                    background={background}
+                    onPress={() => this.refs.input && this.refs.input.focus()}>
+                    <View>
+                        <Image
+                          source={require('./img/android_search_white.png')}
+                          style={styles.icon}
+                        />
+                    </View>
+                </TouchableNativeFeedback>
                 <TextInput 
+                    ref="input"
                     placeholder="Search a house..." 
                     placeholderTextColor="rgba(155, 155, 155, 0.5)"
                     style={styles.searchBarInput}
@@ -25,7 +43,7 @@ let styles = StyleSheet.create({
     searchBar: {
         flexDirection: 'row',
         alignItems: 'center',
-        backgroundColor: 'white',
+        backgroundColor: '#a9a9a9',
         height: 56,
     },
     searchBarInput: {
@@ -34,7 +52,13 @@ let styles = StyleSheet.create({
         fontWeight: 'bold',
         color: 'white',
         height: 50,
+        padding: 0,
         backgroundColor: 'transparent',
+    },
+    icon: {
+        width: 24,
+        height: 24,
+        marginHorizontal: 8,
     },
 });
 

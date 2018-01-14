f80b401b574368e4235f24097fdde064bf28a1d3
zqqiang <qiangzhaoqing@gmail.com>
2018-01-14 15:45:15 -0800

    add main component

--- main.go
+++ main.go
@@ -8,11 +8,7 @@ import (
 func main() {
 	e := echo.New()
 
-	e.File("/", "vue/index.html")
-
-	e.GET("/routes", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.Static("/", "vue")
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
---
+++ vue/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+import Main from './components/Main.vue'
+
+new Vue({
+    components: {
+        Main
+    }
+}).$mount('#app')
